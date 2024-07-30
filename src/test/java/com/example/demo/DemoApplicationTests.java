4d9cd5aafcc40f61f12e62684a2a8ff3f88a0cb0
thanhnd <ducthanhnguyen95@gmail.com>
2024-07-30 23:36:09 +0700

    Deep copy vs shallow copy

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    public void shallowCopyIsNotSameUser() {
+        Address address = new Address("Quang Trung St 6", "Ha Noi", "Viet Nam");
+        User user = new User("Nguyen", "Thanh", address);
+        User shallowCopy = new User(user.getFirstName(), user.getLastName(), user.getAddress());
+        assertThat(shallowCopy).isNotSameAs(user);
+    }
+
+    @Test
+    public void addressShallowCopyIsSameAddress() {
+        Address address = new Address("Quang Trung St 6", "Ha Noi", "Viet Nam");
+        User user = new User("Nguyen", "Thanh", address);
+        User shallowCopy = new User(user.getFirstName(), user.getLastName(), user.getAddress());
+        assertThat(shallowCopy.getAddress()).isSameAs(address);
+    }
+
+    @Test
+    public void countryAddressShallowCopyIsEqualToCountryAddress() {
+        Address address = new Address("Quang Trung St 6", "Ha Noi", "Viet Nam");
+        User user = new User("Nguyen", "Thanh", address);
+        User shallowCopy = new User(user.getFirstName(), user.getLastName(), user.getAddress());
+        address.setCountry("America");
+        assertThat(shallowCopy.getAddress().getCountry()).isEqualTo(user.getAddress().getCountry());
+    }
+
+    @Test
+    public void addressDeepCopyIsNotEqualToAddress() {
+        Address address = new Address("Quang Trung St 6", "Ha Noi", "Viet Nam");
+        User user = new User("Nguyen", "Thanh", address);
+        User deepCopy = new User(user);
+        assertThat(deepCopy.getAddress()).isNotSameAs(address);
+        assertThat(deepCopy.getAddress()).isNotSameAs(user.getAddress());
+    }
+
+    @Test
+    public void countryAddressDeepCopyIsNotEqualToCountryAddress() {
+        Address address = new Address("Quang Trung St 6", "Ha Noi", "Viet Nam");
+        User user = new User("Nguyen", "Thanh", address);
+        User deepCopy = new User(user);
+        address.setCountry("America");
+        assertThat(user.getAddress().getCountry()).isNotEqualTo(deepCopy.getAddress().getCountry());
+    }
+
+
+}
